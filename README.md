e18aba9cf36ba1072c8bfc60661a80e726dde86d
Edin Jakupovic <edin.j.1992@gmail.com>
2024-06-07 19:18:14 +0200

    animate

---
+++ README.md
@@ -0,0 +1,1 @@
+# https://endj.github.io/vigen-re/
--- ui.js
+++ ui.js
@@ -1,9 +1,11 @@
 const A = "A".codePointAt(0)
 const Z = "Z".codePointAt(0)
-const alphabet = new Array(Z - A + 1).fill().map((_, indx) => String.fromCodePoint(indx + A))
+const alphabet = Array.from({ length: Z - A + 1 }, (_, i) => String.fromCodePoint(i + A));
 alphabet.unshift(" ")
 
+
 const body = document.getElementsByTagName("body")[0]
+const encryptedText = document.getElementById("encryptedText")
 const canvas = document.createElement("canvas")
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -11,99 +13,153 @@ canvas.style = "border: 1px solid"
 body.appendChild(canvas)
 const ctx = canvas.getContext("2d");
 
-let fontSize = 50;
+const fontSize = 50;
 ctx.font = `${fontSize}px monospace`;
 const { actualBoundingBoxAscent, actualBoundingBoxDescent } = ctx.measureText("A")
-let textHeight = actualBoundingBoxAscent + actualBoundingBoxDescent;
-
-let cellWidth = canvas.width / alphabet.length;
-let widthPadding = cellWidth * 0.1;
-let textWidth = cellWidth * 0.8;
-
-let heightPadding = textHeight * 0.1;
+const textHeight = actualBoundingBoxAscent + actualBoundingBoxDescent;
+const cellWidth = canvas.width / alphabet.length;
+const widthPadding = cellWidth * 0.1;
+const textWidth = cellWidth * 0.8;
+const heightPadding = textHeight * 0.1;
 
 canvas.height = (textHeight + (heightPadding * 2)) * alphabet.length;
-let cellHeight = canvas.height / alphabet.length;
+const cellHeight = canvas.height / alphabet.length;
+
 ctx.font = `${fontSize}px monospace`;
 
-console.table([
-    ["canvas.height", canvas.height],
-    ["canvas.width", canvas.width],
-    ["textHeight", textHeight],
-    ["padding", widthPadding],
-    ["cellLength", cellWidth],
-    ["textLength", textWidth],
-    ["fontSize", fontSize]
-])
+console.table({
+    "canvas.height": canvas.height,
+    "canvas.width": canvas.width,
+    "textHeight": textHeight,
+    "padding": widthPadding,
+    "cellLength": cellWidth,
+    "textLength": textWidth,
+    "fontSize": fontSize
+});
 
 const drawCharacterAtIndex = (x, y, character) => {
-    if (!character)
+    if (!character) {
         throw "Character is null";
-    if (x < 0 || x > alphabet.length || y < 0 || y > alphabet.length)
+    }
+    if (x < 0 || x > alphabet.length || y < 0 || y > alphabet.length) {
         throw `Invalid args x:${x} y:${y}`
+    }
     ctx.fillText(
         character,
         cellWidth * x + widthPadding,
         cellHeight * y + cellHeight - heightPadding,
-        textWidth);
+        textWidth
+    );
 }
 
-
-ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-// Draw header and sidebar
-for (let x = 0; x < alphabet.length; x++) {
-    drawCharacterAtIndex(x, 0, alphabet[x])
+const highlightCharacterAtIndex = (x, y) => {
+    ctx.save();
+    ctx.fillStyle = "rgb(255,0,0,0.1)";
+    if (x < 0 || x > alphabet.length || y < 0 || y > alphabet.length) {
+        throw `Invalid args x:${x} y:${y}`
+    }
+    ctx.fillRect(
+        cellWidth * x,
+        cellHeight * y,
+        cellWidth,
+        cellHeight
+    );
+    ctx.restore();
 }
-for (let y = 0; y < alphabet.length; y++) {
-    drawCharacterAtIndex(0, y, alphabet[y])
-}
-ctx.moveTo(0, cellHeight)
-ctx.lineTo(canvas.width, cellHeight);
-ctx.stroke()
 
-ctx.moveTo(cellWidth , 0)
-ctx.lineTo(cellWidth, canvas.height)
-ctx.stroke()
-
-// Draw Matrix
-for (let y = 1; y < alphabet.length; y++) {
-    for (let x = 1; x < alphabet.length; x++) {
-        const index = (x + y) % (alphabet.length);
-        if (alphabet[index] === " ") {
-            drawCharacterAtIndex(x, y, alphabet[index + 1])
-        } else {
-            drawCharacterAtIndex(x, y, alphabet[index])
+const drawBoard = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // Draw header and sidebar
+    for (let x = 0; x < alphabet.length; x++) {
+        drawCharacterAtIndex(x, 0, alphabet[x])
+    }
+    for (let y = 0; y < alphabet.length; y++) {
+        drawCharacterAtIndex(0, y, alphabet[y])
+    }
+    ctx.beginPath();
+    ctx.moveTo(0, cellHeight);
+    ctx.lineTo(canvas.width, cellHeight);
+    ctx.moveTo(cellWidth, 0);
+    ctx.lineTo(cellWidth, canvas.height);
+    ctx.stroke();
+    ctx.closePath();
+    // Draw Board
+    for (let y = 0; y < alphabet.length; y++) {
+        for (let x = 1; x < alphabet.length; x++) {
+            const index = (x + y) % (alphabet.length);
+            drawCharacterAtIndex(x, y + 1, alphabet[index] === " " ? alphabet[index + 1] : alphabet[index]);
         }
     }
+    // Draw background highlight shadows
+    ctx.save();
+    ctx.fillStyle = "rgb(0,0,0,0.1)"
+    for (let i = 2; i < alphabet.length; i += 2) {
+        ctx.fillRect(0, cellHeight * i, canvas.width, cellHeight)
+    }
+    ctx.restore();
 }
 
-// Draw background shadows
-ctx.fillStyle = "rgb(0,0,0,0.1)"
-for(let i = 2; i < alphabet.length; i+=2) {
-    ctx.fillRect(0, cellHeight*i, canvas.width, cellHeight)
+const encryptAnimated = (text, key) => {
+    if (!text || !key) {
+        alert("text key null")
+        return
+    }
+    const cryptGenerator = encryptGenerator(text, key)
+    requestAnimationFrame(() => animate(cryptGenerator, null, null))
 }
 
-const encrypt2 = (text,key) => {
-    if(!text || !key) alert("text key null")
-    const generator = encryptGenerator(text, key)
-    requestAnimationFrame(() => animate(generator, 5))
-}
-
-const animate = (generator, iteration) => {
-    if(iteration === 0) return;
-    const nextChar = generator.next().value
-    console.log(nextChar)
-    if(!nextChar) {
+const animate = (currCryptGenerator, currentPathDrawGenerator, highlightGenerator) => {
+    const isHighlighting = highlightGenerator != null && highlightGenerator.next().value
+    if (isHighlighting) {
+        requestAnimationFrame(() => animate(currCryptGenerator, currentPathDrawGenerator, highlightGenerator))
         return;
     }
-        
-    requestAnimationFrame(() => animate(generator, iteration - 1))
+
+    const drawingState = currentPathDrawGenerator !== null && currentPathDrawGenerator.next().value
+    if (drawingState) {
+        if (drawingState[0] === DRAWING) {
+            // Draw and continue
+            requestAnimationFrame(() => animate(currCryptGenerator, currentPathDrawGenerator, highlightGenerator))
+            return;
+        } else if (drawingState[0] === DONE_DRAWING) {
+            const [x, y] = drawingState[1];
+            const highlightGenerator = resultHighlightGenerator(x, y)
+            requestAnimationFrame(() => animate(currCryptGenerator, currentPathDrawGenerator, highlightGenerator))
+            return;
+        }
+    }
+
+    drawBoard()
+    // Get next location to draw
+    const nextChar = currCryptGenerator.next().value
+    if (!nextChar) {
+        return;
+    }
+    const { key, text, char } = nextChar;
+    const textIndex = text.charCodeAt(0) - 65 + 1
+    const keyIndex = key.charCodeAt(0) - 65 + 1
+
+    encryptedText.textContent += char;
+
+    const animationGenerator = drawGenerator(keyIndex, textIndex)
+    requestAnimationFrame(() => animate(currCryptGenerator, animationGenerator))
 }
 
+const DRAWING = 1;
+const DONE_DRAWING = 2;
 
+function* resultHighlightGenerator(x, y) {
+    for (let i = 0; i < 10; i++) {
+        highlightCharacterAtIndex(x, y)
+        yield true;
+    }
+    for (let i = 0; i < 30 /* ~half a second */; i++) {
+        yield true;
+    }
+    yield false;
+}
 
-function* encryptGenerator (text, key) {
+function* encryptGenerator(text, key) {
     for (let i = 0; i < text.length; i++) {
         const keyNum = key[i % key.length].charCodeAt(0) - 65
         const textNum = text[i].charCodeAt(0) - 65
@@ -116,3 +172,17 @@ function* encryptGenerator (text, key) {
     }
     yield undefined;
 }
+
+function* drawGenerator(x, y) {
+    for (let toY = 0; toY <= y; toY++) {
+        highlightCharacterAtIndex(x, toY)
+        yield [DRAWING, [x, y]];
+    }
+    for (let toX = 0; toX <= x; toX++) {
+        highlightCharacterAtIndex(toX, y)
+        yield [DRAWING, [x, y]];
+    }
+    yield [DONE_DRAWING, [x, y]];
+}
+
+drawBoard()
